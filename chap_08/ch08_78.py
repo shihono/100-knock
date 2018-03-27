b8e79e53f07a434191ae500a8ce0e61ad297bd34
shihono <honobono.pika@gmail.com>
2018-03-27 09:13:58 +0900

    add chap_08 ~79

--- chap_08/ch08_78.py
+++ chap_08/ch08_78.py
@@ -1,1 +1,43 @@
 # -*- coding : utf-8 -*-
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LogisticRegression
+from collections import Counter
+from chap_08 import ch08_72, ch08_77
+"78. 5分割交差検定"
+
+
+def make_x_y_text():
+    # データ作成
+    X_text = []
+    y_text = []
+    with open("data/sentiment.txt", 'r')as f:
+        for line in f:
+            stemming_text = ch08_72.get_stemming_text(ch08_72.del_stop_words(line[2:]))
+            X_text.append(dict(Counter(stemming_text)))
+            if line[0] == '+':
+                y_text.append(1)
+            else:
+                y_text.append(0)
+    return X_text, y_text
+
+
+def main():
+    X_text, y_text = make_x_y_text()
+    # K-hold
+    kf = KFold(n_splits=5)
+    kf.get_n_splits(X_text, y_text)
+    for train_idx, test_idx in kf.split(X_text, y_text):
+        lr = LogisticRegression()
+        X_train, y_train = [X_text[idx] for idx in train_idx], [y_text[idx] for idx in train_idx]
+        X_test, y_test = [X_text[idx] for idx in test_idx], [y_text[idx] for idx in test_idx]
+        vec = DictVectorizer(sparse=False)
+        X_train_vec = vec.fit_transform(X_train)
+        X_test_vec = vec.transform(X_test)
+        lr.fit(X_train_vec, y_train)
+        ch08_77.print_accuracy_f1score(y_test, lr.predict(X_test_vec))
+        print("***")
+
+
+if __name__ == '__main__':
+    main()
--- chap_08/ch08_79.py
+++ chap_08/ch08_79.py
@@ -1,1 +1,36 @@
 # -*- coding : utf-8 -*-
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.feature_extraction import DictVectorizer
+import matplotlib.pyplot as plt
+from chap_08 import ch08_78
+"79. 適合率-再現率グラフの描画"
+
+
+def main():
+    X_text,y = ch08_78.make_x_y_text()
+    vec = DictVectorizer(sparse=False)
+    X_array = vec.fit_transform(X_text)
+    X_train, X_test, y_train, y_test = train_test_split(X_array, y, test_size=0.2)
+    lr = LogisticRegression()
+    lr.fit(X_train, y_train)
+    y_score = lr.decision_function(X_test)
+    average_precision = average_precision_score(y_test, y_score)
+    precision, recall, _ = precision_recall_curve(y_test, y_score)
+
+    # グラフ設定
+    plt.step(recall, precision, color='b', alpha=0.2, where='post')
+    plt.fill_between(recall, precision, step='post', alpha=0.2, color='b')
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('2-class Precision-Recall curve: AUC={0:0.2f}'.format(average_precision))
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()

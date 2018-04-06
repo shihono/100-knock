5c3f75b97d25b3cab42ecfbf549ac05e547b4d3b
shihono <honobono.pika@gmail.com>
2018-04-06 16:41:23 +0900

    changechap10 91

--- chap_10/ch10_91.py
+++ chap_10/ch10_91.py
@@ -3,6 +3,14 @@ from itertools import takewhile, dropwhile
 "91. アナロジーデータの準備"
 
 
+def main2():
+    # takewhileとdropwhileを使う場合
+    with open('./data/questions-words.txt', 'r')as f:
+        for line in takewhile(lambda y: y == ': family\n' or not y.startswith(':'),
+                              dropwhile(lambda x: x != ': family\n', f)):
+            print(line.strip())
+
+
 def main():
     flag = False
     with open('./work/91_analogy.txt', 'w')as fw:
